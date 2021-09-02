88d41fd8f9734c56183bc5b23fd0cabf3c3b32db
이충헌 <ccik2285@naver.com>
2021-09-02 19:29:49 +0900

    test commit 은ㅇ뉴

--- app/src/main/java/com/minew/beaconset/demo/Optimal_Distance.java
+++ app/src/main/java/com/minew/beaconset/demo/Optimal_Distance.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+//시험한번만 해보겔요
 public class Optimal_Distance{
     public static int path[] = new int[5];
     static int width;
