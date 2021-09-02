d9b828fac1fbe15bb292bb4d903462d019481de8
배정현 <bae1004kin@likelion.org>
2021-09-02 15:12:08 +0900

    //배정현//QOWJ

--- app/src/main/java/com/minew/beaconset/demo/SubActivity.java
+++ app/src/main/java/com/minew/beaconset/demo/SubActivity.java
@@ -45,6 +45,8 @@ import java.util.Collections;
 import java.util.List;
 
 //배정현
+//HIHI
+
 public class SubActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = "AnimationActivity";
