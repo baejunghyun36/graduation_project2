0f19d4d5da311dd481655d2dad979fa370fa3b29
이충헌 <ccik2285@naver.com>
2021-09-02 17:37:30 +0900

    ㅅㄷㄴㅅ22

--- app/src/main/java/com/minew/beaconset/demo/SubActivity.java
+++ app/src/main/java/com/minew/beaconset/demo/SubActivity.java
@@ -52,6 +52,7 @@ import java.util.List;
 //정현 5:14
 //정현 5:19
 //마이크테스트
+//원ㄷ투원투
 public class SubActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = "AnimationActivity";
