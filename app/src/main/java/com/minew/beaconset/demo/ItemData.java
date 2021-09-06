0f1ebd8e4ab82d1eda064f61910710ab4436cd5b
wordcount <choiey@naver.com>
2021-09-06 17:04:24 +0900

    items push 장바구니 클래스로 연결X

---
+++ app/src/main/java/com/minew/beaconset/demo/ItemData.java
@@ -0,0 +1,27 @@
+package com.minew.beaconset.demo;
+
+public class ItemData {
+    private int iv_profile; //이미지 뷰
+    private String item_name;   //아이템 이름
+
+    public ItemData(int iv_profile, String item_name) {
+        this.iv_profile = iv_profile;
+        this.item_name = item_name;
+    }
+
+    public int getIv_profile() {
+        return iv_profile;
+    }
+
+    public void setIv_profile(int iv_profile) {
+        this.iv_profile = iv_profile;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+}
