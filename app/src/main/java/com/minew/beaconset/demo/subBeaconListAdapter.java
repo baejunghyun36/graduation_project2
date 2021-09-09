766c1e0e5ee49e2bf372152c9a5f49b3a0c7aebe
이충헌 <ccik2285@naver.com>
2021-09-09 16:13:39 +0900

    recycler 삭제

--- app/src/main/java/com/minew/beaconset/demo/subBeaconListAdapter.java
+++ app/src/main/java/com/minew/beaconset/demo/subBeaconListAdapter.java
@@ -29,15 +29,15 @@ public class subBeaconListAdapter extends RecyclerView.Adapter<subBeaconListAdap
 
     public interface OnItemClickLitener {
         void onItemClick(View view, int position);
+
         void onItemLongClick(View view, int position);
     }
 
     private OnItemClickLitener mOnItemClickLitener;
+
     public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
         this.mOnItemClickLitener = mOnItemClickLitener;
     }
-
-
 
 
     @Override
@@ -49,39 +49,16 @@ public class subBeaconListAdapter extends RecyclerView.Adapter<subBeaconListAdap
 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
-        holder.setDataAndUi(mMinewBeacons.get(position));
-
-        if(Float.parseFloat(mMinewBeacons.get(position).getMinor())==56388){
-            i[0] = mMinewBeacons.get(position).getDistance();;
-        }
-        else if(Float.parseFloat(mMinewBeacons.get(position).getMinor())==56877){
+        if (Float.parseFloat(mMinewBeacons.get(position).getMinor()) == 56388) {
+            i[0] = mMinewBeacons.get(position).getDistance();
+        } else if (Float.parseFloat(mMinewBeacons.get(position).getMinor()) == 56877) {
             i[1] = mMinewBeacons.get(position).getDistance();
-        }
-        else if(Float.parseFloat(mMinewBeacons.get(position).getMinor())==56878){
-            i[2]= mMinewBeacons.get(position).getDistance();
-        }
-        else {
+        } else if (Float.parseFloat(mMinewBeacons.get(position).getMinor()) == 56878) {
+            i[2] = mMinewBeacons.get(position).getDistance();
+        } else {
             i[3] = mMinewBeacons.get(position).getDistance();
         }
-
-        if (mOnItemClickLitener != null) {
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int pos = holder.getLayoutPosition();
-                    mOnItemClickLitener.onItemClick(holder.itemView, pos);
-                }
-            });
-
-            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    int pos = holder.getLayoutPosition();
-                    mOnItemClickLitener.onItemLongClick(holder.itemView, pos);
-                    return false;
-                }
-            });
-        }
+        holder.itemView.setVisibility(View.GONE);
     }
 
     @Override
@@ -106,74 +83,21 @@ public class subBeaconListAdapter extends RecyclerView.Adapter<subBeaconListAdap
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         private MinewBeacon mMinewBeacon;
-//        private final TextView mDevice_name;
-//        private final TextView mDevice_uuid;
         private final TextView mDevice_other;
-//        private final TextView mConnectable;
         private final TextView mDevice_location;
         private final TextView mDevice_who;
         private final TextView distance1;
-//        private final TextView hi1;
 
 
         public MyViewHolder(View itemView) {
             super(itemView);
-//            mDevice_name = (TextView) itemView.findViewById(R.id.device_name);
-//            mDevice_uuid = (TextView) itemView.findViewById(R.id.device_uuid);
             mDevice_other = (TextView) itemView.findViewById(R.id.device_other);
-//            mConnectable = (TextView) itemView.findViewById(R.id.device_connectable);
             mDevice_location = (TextView) itemView.findViewById(R.id.device_location);
             mDevice_who = (TextView) itemView.findViewById(R.id.who_device);
-            distance1= (TextView) itemView.findViewById(R.id.distance1);
-//            hi1= (TextView) itemView.findViewById(R.id.hi);
+            distance1 = (TextView) itemView.findViewById(R.id.distance1);
 
         }
 
 
-        public void setDataAndUi(MinewBeacon minewBeacon) {
-            mMinewBeacon = minewBeacon;
-
-
-
-            String format = String.format("Minor:%s Rssi:%s Distance:%s ",
-                     mMinewBeacon.getMinor(),
-                    mMinewBeacon.getRssi(),
-                    mMinewBeacon.getDistance()
-                    );
-
-            mDevice_other.setText(format);
-
-            String form1= String.format("%s",mMinewBeacon.getDistance());
-
-            distance1.setText(form1);
-
-
-            String s1 ="56388";
-            String s2="56877";
-            String s3= "56878";
-
-            if(s1.equals(mMinewBeacon.getMinor())){
-                mDevice_location.setText("지하주차장A");
-                mDevice_who.setText("정현이꺼");
-                location = "정현이꺼";
-            }
-            else if(s2.equals(mMinewBeacon.getMinor())){
-                mDevice_location.setText("지하주차장B");
-                mDevice_who.setText("은윤이꺼");
-                location = "은윤이꺼";
-            }
-            else if(s3.equals(mMinewBeacon.getMinor())){
-                mDevice_location.setText("지하주차장C");
-                mDevice_who.setText("충헌이꺼");
-                location = "충헌이꺼";
-            }
-
-            distance = Float.toString(minewBeacon.getDistance());
-
-
-
-        }
     }
-
-
 }
